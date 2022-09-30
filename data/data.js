309be58b32cb2f1dfd774a129f62975423cf5ca1
BB-SeanF <107635536+BB-SeanF@users.noreply.github.com>
2022-09-30 11:09:41 -0400

    Presentation Notes Update 2 and updates to HP images

--- data/data.js
+++ data/data.js
@@ -65,11 +65,11 @@ export const projectFrames = [
         
         <p>The next section is specific for curriculum, with large links for each of the different school levels. A big note that was repeated here was just how boxy the current site feels. This new design aims to break out of that contained feel with something more open and elegant. These three links, when hovered over, will spring to life with color, though this is shown on the next slide.</p> 
         
-        <p>The next section’s layout mirrors the introduction region above, but also provides something new for USN. This section will allow you to feature different elements of the school on the homepage. While “Traditions” “Go Tigers!” etc are listed here, these options can be really any facet of the school you wish to highlight. When a user selects one of these, both the description text <em> and image will change. Historically this was not achievable on the platform, though we have recently opened the door to some small custom coding to fill in product gaps as we can. “Tabbed” content is requested a fair amount, and one we can solve for now with this approach.</p> 
+        <p>The next section’s layout mirrors the introduction region above, but also provides something new for USN. This section will allow you to feature different elements of the school on the homepage. While “Traditions” “Go Tigers!” etc are listed here, these options can be really any facet of the school you wish to highlight. When a user selects one of these, both the description text <em> and </em> image will change. Historically this was not achievable on the platform, though we have recently opened the door to some small custom coding to fill in product gaps as we can. “Tabbed” content is requested a fair amount, and one we can solve for now with this approach.</p> 
         
         <p>I am closing out the homepage with a news and event section. We are also contrasting the rest of the layout to have this further stand out as its own section. With news first we see one, large, featured story along with three other headlines and photos, allowing visitors to get a glimpse into the recent happenings around the school. Below all of this is a horizontal events carousel</p> 
         
-        <p>The site’s footer rounds out this and all pages of the site. CTA links are repeated here (and therefore will appear on every page in a non-intrusive manner). The formal USN and Seal logo also is included here. The rest of the footer includes the standard fare, with the address, resource links, social media icons, and boilerplate text.</p>`, 
+        <p>The site’s footer rounds out this and all pages of the site. CTA links are repeated here (and therefore will appear on every page in a non-intrusive manner). The formal USN and Seal logo also is included here. The rest of the footer includes the standard fare, with the address, resource links, social media icons, and boilerplate text.</p>` , 
     },
     {
         id:2,
@@ -80,18 +80,20 @@ export const projectFrames = [
         frameGroup:1,
         navGraphic: true,
         navGraphicToChange: true,
-        description:"",//"<p>With Accessability being a cheif concern, care went into how interactivty would be handled on the site, as that's where most of accessability pitfalls occur. Additionally, with this being a fairly static mock up presentation, it can be difficult to preview all of the different interactivity and microinteractions that the design will have. The plan is to have each of the elements on the HP transition in very similar to the referenced Bentley School, or other blackbaud schools with that feature applied. While we can discuss this further, I did opt for a simple drop down menu with accessiblity being the largest factor. If a mega menu is truely desired, we can loop back to this and enhance what's here.</p>",
+        description:`<p>This slide may look similar to the first, however the significant difference here is how interactive items like links show their hover states. Again, a bit limited in the static nature of these presentations, we, buttons like “Apply” or “Learn More” will have their text slide off to the side, while an arrow will slide in to take their place, bringing some visual interest as a user interacts with items.</p> 
+
+        <p>The curriculum section shows how each of the large links will appear when hovered as well, though all three are triggered here to show how they are both different colors, and a different image can be attached as their backgrounds.</p>`, 
     },
    {
         id:3,
         img:"/images/hp3.jpg",
         height:"900px",
         width: "1600px",
-        title: "Main Nav Panel Preview",
+        title: "Navigation Interaction Preview",
         frameGroup:1,
         navGraphic: false,
         navGraphicToChange: false,
-        description:"",//"<p>Shown here are the site's two navigations, one for the main nav and the other a more utility nav. The main nav is composed of a simple drop down, clean and effective without any major fuss, it should quickly let visitors navigate the site without overwhelming them.</p><p>The other navigation has been set up like a utility panel and is meant for the current community members. In there they will find all of the links to the various portal logins, as well as any helpful public pages like the school calendar, or main news page. Search is also tucked in there. This both consolidates all of these more community focused resources into one place, as well makes it a bit easier to retrain said community on where to go on the site.</p>",
+        description:`<p>Shown here is how USN’s new expanded navigation will look when an item is hovered over. For your reference, this slide does not scroll, instead consider this a snapshot of how nav elements will appear. The biggest item to note here is the Mega Menu panel dropdown. Shown here is the curriculum panel, though all panels will follow the same scheme, with a section for a header and text, and button. The main nav items in the center, and an associated photo on the far right.</p>`, 
     },
     {
         id:4,
@@ -102,7 +104,7 @@ export const projectFrames = [
         frameGroup:2,
         navGraphic: true,
         navGraphicToChange: true,
-        description:``,//`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The first of these layouts has been dubbed "Landing Page" for it's overall scope of cotnent and somewhat centered layout that would do well as an introduction page to a section.</p>`,
+        description:`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The first of these layouts has been dubbed "Landing Page" for it's overall scope of cotnent and somewhat centered layout that would do well as an introduction page to a section.</p>`,
     },
     {
         id:5,
@@ -113,7 +115,8 @@ export const projectFrames = [
         frameGroup:2,
         navGraphic: true,
         navGraphicToChange: true,
-        description:``,//`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The second layout is aptly named the "Wide Left, Skinny Right" layout and is by far one of the most common layout schemes across websites. Great for pages whose content, while important, doesn't need to be as engaging as a landing page or other pages.</p>`,
+        description:`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The second layout is aptly named the "Wide Left, Skinny Right" layout and is by far one of the most common layout schemes across websites. Great for pages whose content, while important, doesn't need to be as engaging as a landing page or other pages.</p> 
+        <p>Also, immediately apparent here is the lack of a page banner. We understand that sometimes, finding the right or relevant page banner for every single page can be difficult. This type of page "banner" region is another solution for pages that don't actually need a banner, like forms, or deeper sub pages. You may not use this banner design a lot, but it's there for when you need it.</p>`,
     },
     {
         id:6,
@@ -124,7 +127,7 @@ export const projectFrames = [
         frameGroup:2,
         navGraphic: true,
         navGraphicToChange: true,
-        description:``,//`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The last of the provided samples, the "Mixed Row Layout" showcases the flexibilty you'll have with the design. While a landing page isn't right for every page, and a simple layout might not do more engaging content justice, something in the middle might serve a page better.</p><p>Also, immediately apparent here is the lack of a page banner. We understand that sometimes, finding the right or relevant page banner for every single page can be difficult. This type of page "banner" region is another solution for pages that don't actually need a banner, like forms, or deeper sub pages. You may not use this banner design a lot, but it's there for when you need it.</p>`,
+        description:`<p>A school website is more than just the homepage, and as such this presentation includes three different sample interior layouts to preview. Each of these vary in scope and content (though they all use placeholder content), but they are all meant to showcase how interior pages can look with this new site design.</p><p>The last of the provided samples, the "Varied Content" showcases the flexibilty you'll have with the design. While a landing page isn't right for every page, and a simple layout might not do more engaging content justice, something in the middle might serve a page better.</p>`,
     },
      /*{
         id:7,
